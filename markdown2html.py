c28d2c35729bb06de1a5bc0542121ec83afc7ee3
Louis Martinez Echevarria <louismartinezechevarria@gmail.com>
2024-12-11 15:27:14 -0400

    0.2

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Markdown to HTML"""
+
 import os
 import sys
+
 if (len(sys.argv) < 3):
     sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
     exit(1)
+
 if not (os.path.isfile(sys.argv[1])):
     sys.stderr.write("Missing " + sys.argv[1])
     exit(1)
+
 exit(0)
